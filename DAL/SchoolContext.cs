f342cbedddc2624f0d9071d8996a82939f0c03e7
Jeremy Tucker <Jeremytucker76@gmail.com>
2023-04-25 14:25:30 -0400

    student context fixed

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+
+namespace ContosoUniversity.DAL
+{
+	public class SchoolContext : DbContext
+	{
+		public SchoolContext() : base("SchoolContext")
+		{
+		}
+
+		public DbSet<Student> Students { get; set; }
+		public DbSet<Enrollment> Enrollments { get; set; }
+		public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
+
